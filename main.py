4459e37fa4eddf534de89ad672abd1e8a282c623
davidmoraleees <davidmoralesdeleon@gmail.com>
2024-07-06 13:42:40 +0200

    Comments added

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import io, filters, morphology, measure, color
+from skimage import filters, morphology, measure, color
 from scipy.ndimage import distance_transform_edt
 from tqdm import tqdm 
 from sklearn.cluster import KMeans
 
-
-filename_cc = '53581264_80123a24997098dc_MG_R_CC_ANON.png' #CC image of a right breast
-filename_mlo = '53581237_80123a24997098dc_MG_R_ML_ANON.png' #MLO image of a right breast
-
-id_image = filename_cc[:-17]
-
-cc_image_path = os.path.join('INbreast/AllDICOMs_PNG', filename_cc)
-mlo_image_path = os.path.join('INbreast/AllDICOMs_PNG', filename_mlo)
-
-cc_image = plt.imread(cc_image_path)
-mlo_image = plt.imread(mlo_image_path)
-
 ''''
 INDEX 
+0. General configurations and image loading
 1. Breast periphery separation
 2. Intensity ratio propagation
 3. Breast thickness estimation
@@ -27,43 +16,48 @@ INDEX
 5. Breast segmentation
 '''
 
-# 1. Breast periphery separation ------------------------------------------------------------------------------
-def separate_periphery(image): # Function to separate the breast peripheral area (BPA) using Otsu thresholding
-    # Threshold method 1
-    otsu_thresh = filters.threshold_otsu(image) # analyzes the image histogram and calculates the optimal intensity threshold using Otsu's method.
-    bpa_otsu = image > otsu_thresh # BPA's binary image using Otsu's thresholding method
-
-    # Threshold method 2
-    mean_intensity = image[bpa_otsu].mean() # Calculate the mean intensity of the breast tissue
-    bpa_threshold = image > mean_intensity # BPA's binary image using the mean intensity thresholding method
-
-    # Combination of both mehtods  
-    bpa_combined = np.logical_or(bpa_otsu, bpa_threshold) # Combine the two thresholding methods to get a better separation (True if at least one of the thresholding methods considers it as part of the foreground)
-    bpa_filled = morphology.remove_small_holes(bpa_combined, area_threshold=64) # Fill small holes in the binary image (for areas =< 64 pixels)
-    bpa_dilated = morphology.binary_dilation(bpa_filled, morphology.square(3)) # Dilate the binary image to include boundary pixels 
-
-    # Finding the largest region, which corresponds to the breast
-    labeled_bpa = measure.label(bpa_dilated) # Label connected regions in the binary image
-    regions = measure.regionprops(labeled_bpa) # Calculates the properties of the labeled regions in the image
-    largest_region = max(regions, key=lambda r: r.area) # Finds the largest region, which should correspond to the breast
-
-    # bpa_final : The final binary image contains only the largest region, which is assumed to be breast tissue.
-    bpa_final = np.zeros_like(bpa_combined)
-    bpa_final[labeled_bpa == largest_region.label] = 1  # Assigns a true value to the pixels that belong to the largest region in the labeled image
-    
-    # Finds the breast tissue contour
-    pb = measure.find_contours(bpa_final, 0.5)[0] # Find the contour of the breast periphery 
-    return bpa_final, pb
-
-cc_bpa, cc_pb = separate_periphery(cc_image)
-mlo_bpa, mlo_pb = separate_periphery(mlo_image)
+# 0. General configurations and image loading ---------------------------------------------------------------------
+os.environ['LOKY_MAX_CPU_COUNT'] = '4' # Maximum number of CPU cores. This is set to avoid issues with detecting physical cores
 
 def axis_off(): # Function to configure axis plots
     plt.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False, labelbottom=False, labelleft=False)
 
-output_dir = 'Output_images'
+output_dir = 'Output_images_proves'
 if not os.path.exists(output_dir): # We make sure that the output images folder exists
     os.makedirs(output_dir)  
+
+filename_cc = '53581264_80123a24997098dc_MG_R_CC_ANON.png' # CC image of a right breast
+filename_mlo = '53581237_80123a24997098dc_MG_R_ML_ANON.png' # MLO image of a right breast
+
+id_image = filename_cc[:-17] # ID to identify every patient
+
+cc_image_path = os.path.join('INbreast/AllDICOMs_PNG', filename_cc)
+mlo_image_path = os.path.join('INbreast/AllDICOMs_PNG', filename_mlo)
+
+cc_image = plt.imread(cc_image_path)
+mlo_image = plt.imread(mlo_image_path)
+
+
+# 1. Breast periphery separation ------------------------------------------------------------------------------
+def separate_periphery(image):
+    """Function to separate the breast peripheral area (BPA)"""
+    otsu_thresh = filters.threshold_otsu(image) # Optimal intensity threshold using Otsu's method
+    bpa_otsu = image > otsu_thresh # BPA's binary otsu image
+    mean_intensity = image[bpa_otsu].mean() # Mean intensity of the breast tissue
+    bpa_threshold = image > mean_intensity # BPA's binary image using the mean intensity thresholding method 
+    bpa_combined = np.logical_or(bpa_otsu, bpa_threshold) # Combination of the two thresholding methods 
+    bpa_filled = morphology.remove_small_holes(bpa_combined, area_threshold=64) # Fill small holes in the binary image (for areas =< 64 pixels)
+    bpa_dilated = morphology.binary_dilation(bpa_filled, morphology.square(3)) # Dilate the binary image to include boundary pixels 
+    labeled_bpa = measure.label(bpa_dilated) # Label connected regions in the binary image
+    regions = measure.regionprops(labeled_bpa) # Properties of the labeled regions in the image
+    largest_region = max(regions, key=lambda r: r.area) # Largest region, which should correspond to the breast
+    bpa_final = np.zeros_like(bpa_combined)
+    bpa_final[labeled_bpa == largest_region.label] = 1  # Assigns a true value to the pixels that belong to the largest region in the labeled image
+    pb = measure.find_contours(bpa_final, 0.5)[0] # Contour of the breast periphery 
+    return bpa_final, pb
+
+cc_bpa, cc_pb = separate_periphery(cc_image)
+mlo_bpa, mlo_pb = separate_periphery(mlo_image)
 
 plt.figure(figsize=(6, 6))
 plt.subplot(2, 2, 1)
@@ -94,23 +88,22 @@ plt.show()
 
 
 # 2. Intensity ratio propagation ---------------------------------------------------------------------------------
-def intensity_ratio_propagation(image, periphery, neighborhood_size): # Function to propagate intensity ratio for correcting intensity variations
-    distance_map = distance_transform_edt(periphery) # Calculate the distance transform of the periphery
+def intensity_ratio_propagation(image, periphery, neighborhood_size): 
+    """Function to propagate intensity ratio for correcting intensity variations"""
     corrected_image = image.copy()
     rows, cols = image.shape
     half_size = neighborhood_size // 2
 
-    for y in tqdm(range(rows)):
-        for x in range(cols):
-            if periphery[y, x]:
-                ymin = max(0, y-half_size)
-                ymax = min(rows, y+half_size+1)
-                xmin = max(0, x-half_size)
-                xmax = min(cols, x+half_size+1)
-            
-                neighborhood = image[ymin:ymax, xmin:xmax] 
-                local_ratio = np.mean(neighborhood) / (image[y, x] + 1e-8) #We add 1e-8 to avoid division by zero.
-                corrected_image[y, x] *= local_ratio
+    for x in tqdm(range(rows)):
+        for y in range(cols):
+            if periphery[x, y]: # Check if the current pixel is part of the periphery
+                xmin = max(0, x-half_size) # Minimum x-coordinate of the neighborhood
+                xmax = min(rows, x+half_size+1) # Maximum x-coordinate of the neighborhood
+                ymin = max(0, y-half_size)  # Minimum y-coordinate of the neighborhood
+                ymax = min(cols, y+half_size+1)  # Maximum y-coordinate of the neighborhood
+                neighborhood = image[xmin:xmax, ymin:ymax] # Local neighborhood extraction around the current pixel
+                local_ratio = np.mean(neighborhood) / (image[x, y] + 1e-8) #We add 1e-8 to avoid division by zero.
+                corrected_image[x, y] *= local_ratio # Adjust the intensity of the current pixel by the local ratio
     
     return corrected_image
 
@@ -144,11 +137,12 @@ plt.show()
 
 
 # 3. Breast thickness estimation ---------------------------------------------------------------------------------
-# From now on, it is assumed that we have a right breast image, so the chest wall is placed at the right edge.
-def find_furthest_point_from_chest_wall(skinline, image_width): # The point found will be close to the nipple.
-    chest_wall_x = image_width - 1  # Chest wall x-coordinate (right edge of image).
-    distances = chest_wall_x - skinline[:, 1] # vector containing the horizontal distances from the chest wall to each point on the skin line
-    furthest_point_index = np.argmax(distances) # Find the index of the point with the maximum distance.
+def find_furthest_point_from_chest_wall(skinline, image_width): 
+    """Function to find the furthest point from the chest wall. It will typically be near the nipple.
+       Skinline is used as a synonym of contour in this script"""
+    chest_wall_x = image_width - 1  # Chest wall x-coordinate (right edge of image)
+    distances = chest_wall_x - skinline[:, 1] # Vector containing the horizontal distances from the chest wall to each point on the skinline
+    furthest_point_index = np.argmax(distances) # Finds the index of the point with the maximum distance
     furthest_point = skinline[furthest_point_index] # Returns the furthest point and its index
     return furthest_point, furthest_point_index
 
@@ -157,52 +151,57 @@ furthest_point_cc, furthest_point_index_cc = find_furthest_point_from_chest_wall
 image_width_mlo = mlo_image.shape[1]
 furthest_point_mlo, furthest_point_index_mlo = find_furthest_point_from_chest_wall(mlo_pb, image_width_mlo)
 
-mlo_pb_upper = mlo_pb[:furthest_point_index_mlo + 1]
+mlo_pb_upper = mlo_pb[:furthest_point_index_mlo + 1] # We divide the skinline into an upper skinline and a lower skinline based on the furthest point
 mlo_pb_lower = mlo_pb[furthest_point_index_mlo:]
 
 plt.figure(figsize=(6,6))
 plt.imshow(mlo_bpa, cmap='gray')
-plt.plot(mlo_pb_upper[:, 1], mlo_pb_upper[:, 0], '-b', linewidth=2)
-plt.plot(mlo_pb_lower[:, 1], mlo_pb_lower[:, 0], '-g', linewidth=2)
-plt.plot(furthest_point_mlo[1], furthest_point_mlo[0], 'yo')
+plt.plot(mlo_pb_upper[:, 1], mlo_pb_upper[:, 0], '-b', linewidth=2) # Plot of the x-y coordinates of the upper skinline
+plt.plot(mlo_pb_lower[:, 1], mlo_pb_lower[:, 0], '-g', linewidth=2) # Plot of the x-y coordinates of the lower skinline
+plt.plot(furthest_point_mlo[1], furthest_point_mlo[0], 'yo') # Plot of the x-y coordinates of the furthest point
 plt.title('MLO Peripheral area')
 axis_off()
 plt.tight_layout()
 plt.savefig(os.path.join(output_dir, f'MLO_peripheral_area_{id_image}.png'))
 plt.show()
 
-def find_nearest_top(skinline): # Function to find the closest point to the top edge of the image.
+# Now we have to generate a set of parallel lines. For this reason, various functions are defined:
+def find_nearest_top(skinline):
+    """Function to find the closest point to the top edge of the image"""
     top_point_index = np.argmin(skinline[:, 0])
     return skinline[top_point_index]
 
-def find_nearest_right(skinline): #Function to find the closest point to the right edge of the image.
+def find_nearest_right(skinline):
+    """Function to find the closest point to the right edge of the image"""
     right_point_index = np.argmax(skinline[:, 1])
     return skinline[right_point_index]
 
-def calculate_slope(point1, point2): #Function to calculate the slope between two points.
+def calculate_slope(point1, point2):
+    """Function to calculate the slope between two points"""
     return (point2[0] - point1[0]) / (point2[1] - point1[1])
 
-def find_intersection(skinline, slope, intercept): # Finds the point of intersection of a line with the skin line.
+def find_intersection(skinline, slope, intercept):
+    """Function to find the closest point of intersection of a line with the skinline"""
     for y, x in skinline:
-        if np.isclose(y, slope * x + intercept, atol=1.0):
+        if np.isclose(y, slope * x + intercept, atol=1.0): # atol is the absolute tolerance
             return [y, x]
     return None
 
-thickest_point_cc=np.copy(furthest_point_cc)
-thickest_point_cc[1]+=180. # We add some distance so we are not on the furthest point, now we are on the thickest point.
+thickest_point_cc=np.copy(furthest_point_cc) # The thickest point is needed to generate the parallel lines
+thickest_point_cc[1]+=180. # Adding some arbitrary distance so we are not on the furthest point, but on the thickest point
 thickest_point_mlo=np.copy(furthest_point_mlo)
-thickest_point_mlo[1]+=180. 
+thickest_point_mlo[1]+=180. # Adding some arbitrary distance so we are not on the furthest point, but on the thickest point
 
 def draw_reference_and_parallel_lines(image, skinline, offset_distance, num_lines, thickest_point, output_dir, id_image):
-    top_reference = find_nearest_top(skinline)
-    right_reference = find_nearest_right(skinline)
+    top_reference = find_nearest_top(skinline) # Nearest point to the top boundary of the image
+    right_reference = find_nearest_right(skinline) # Nearest point to the right boundary of the image
     
-    slope = calculate_slope(top_reference, right_reference)
-    intercept = top_reference[0] - slope * top_reference[1] 
+    slope = calculate_slope(top_reference, right_reference) # Slope of the line connecting top_reference and right_reference
+    intercept = top_reference[0] - slope * top_reference[1] # Intercept of the line
     
-    parallel_lines = []
+    parallel_lines = [] # Initialization of the variable.
     min_distance = float(10**8) # Initialization of the variable.
-    closest_line = None
+    closest_line = None # Variable to store the closest parallel line to the thickest point.
 
     plt.figure(figsize=(6, 6))
     plt.subplot(1, 2, 1)
@@ -210,7 +209,7 @@ def draw_reference_and_parallel_lines(image, skinline, offset_distance, num_line
     
     for i in tqdm(range(num_lines)):
         parallel_intercept = intercept - (i + 1) * offset_distance / np.cos(np.arctan(slope))
-        parallel_top = find_intersection(skinline, slope, parallel_intercept)
+        parallel_top = find_intersection(skinline, slope, parallel_intercept) # Find intersection points of the parallel line with the skinline and its reverse
         parallel_bottom = find_intersection(skinline[::-1], slope, parallel_intercept)
         
         if parallel_top is not None and parallel_bottom is not None:
@@ -248,8 +247,8 @@ def draw_reference_and_parallel_lines(image, skinline, offset_distance, num_line
 
     return parallel_lines, closest_line
 
-offset_distance = -1 #Negative value means going left.
-num_lines = len(mlo_pb_upper) #We want the upper skinline to match one point of the lower skinline.
+offset_distance = -1 #Negative value means going left in the image.
+num_lines = len(mlo_pb_upper) #We want every point in the upper skinline to match one point of the lower skinline.
 parallel_lines, closest_line = draw_reference_and_parallel_lines(mlo_bpa, mlo_pb, offset_distance, num_lines, thickest_point_mlo, output_dir, id_image)
 
 def calculate_length(line):
